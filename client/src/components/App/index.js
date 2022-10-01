8a013e1fef03b340aecc4cdd613fb557dc087f11
Rahul Sharma <rahulsharma@Rahuls-MacBook-Air-4.local>
2022-10-01 18:58:52 -0400

    FEAT: Client Bootstrap - CRA, SASS, MUI, Router

---
+++ client/src/components/App/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+
+// Styles
+import "../../styles/app.scss";
+
+// Components
+import About from "../About";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Paper className="app__title">NetWorthy-App</Paper>
+      <br />
+      <Router>
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
