d0126807322306d840851398c13f725c9768466d
= <amolghar@buffalo.edu>
2022-10-13 18:23:26 -0400

    CORS Issue Fix 1

--- networthy/src/main/java/com/ub/networthy/NetWorthyApplication.java
+++ networthy/src/main/java/com/ub/networthy/NetWorthyApplication.java
@@ -2,6 +2,8 @@ package com.ub.networthy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,6 +13,15 @@ public class NetWorthyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetWorthyApplication.class, args);
+	}
+	
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 }
