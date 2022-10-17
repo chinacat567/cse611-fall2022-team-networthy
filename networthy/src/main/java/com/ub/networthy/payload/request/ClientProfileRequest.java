a5cd6c4c210c2469efbe44e5fa1b856933c9eacb
Sumantra Sharma <sumantra@buffalo.edu>
2022-10-17 10:05:38 -0400

    reverting changes to ClientProfile/ClientProfileRequest

--- networthy/src/main/java/com/ub/networthy/payload/request/ClientProfileRequest.java
+++ networthy/src/main/java/com/ub/networthy/payload/request/ClientProfileRequest.java
@@ -1,189 +1,187 @@
 package com.ub.networthy.payload.request;
 
+import java.sql.Date;
+
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.Date;
-
 public class ClientProfileRequest {
 
-    @NotBlank
-    private String username;
+	@NotBlank
+	private String username;
 
-    @NotBlank
-    private String emailId;
+	@NotBlank
+	private String emailId;
 
-    @NotBlank
-    private String firstName;
+	@NotBlank
+	private String firstName;
 
-    @NotBlank
-    private String lastName;
+	@NotBlank
+	private String lastName;
 
-    @NotBlank
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
-    private Date dateOfBirth;
+	@NotBlank
+	private Date dateOfBirth;
 
-    @NotBlank
-    private String gender;
+	@NotBlank
+	private String gender;
 
-    @NotBlank
-    private String occupation;
+	@NotBlank
+	private String occupation;
 
-    @NotBlank
-    private String education;
+	@NotBlank
+	private String education;
 
-    @NotBlank
-    private String university;
+	@NotBlank
+	private String university;
 
-    @NotBlank
-    private String location;
+	@NotBlank
+	private String location;
 
-    @NotBlank
-    private int financialLevel;
+	@NotBlank
+	private int financialLevel;
 
-    @NotBlank
-    private String learningMethod;
+	@NotBlank
+	private String learningMethod;
 
-    @NotBlank
-    private int income;
+	@NotBlank
+	private int income;
 
-    @NotBlank
-    private int debt;
+	@NotBlank
+	private int debt;
 
-    /* General field can be blank */
-    private String general;
+	@NotBlank
+	private String general;
 
-    @NotBlank
-    private boolean profileStatus;
+	@NotBlank
+	private boolean profileStatus;
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getEmailId() {
-        return emailId;
-    }
+	public String getEmailId() {
+		return emailId;
+	}
 
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
-    public String getOccupation() {
-        return occupation;
-    }
+	public String getOccupation() {
+		return occupation;
+	}
 
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
 
-    public String getEducation() {
-        return education;
-    }
+	public String getEducation() {
+		return education;
+	}
 
-    public void setEducation(String education) {
-        this.education = education;
-    }
+	public void setEducation(String education) {
+		this.education = education;
+	}
 
-    public String getUniversity() {
-        return university;
-    }
+	public String getUniversity() {
+		return university;
+	}
 
-    public void setUniversity(String university) {
-        this.university = university;
-    }
+	public void setUniversity(String university) {
+		this.university = university;
+	}
 
-    public String getLocation() {
-        return location;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-    public int getFinancialLevel() {
-        return financialLevel;
-    }
+	public int getFinancialLevel() {
+		return financialLevel;
+	}
 
-    public void setFinancialLevel(int financialLevel) {
-        this.financialLevel = financialLevel;
-    }
+	public void setFinancialLevel(int financialLevel) {
+		this.financialLevel = financialLevel;
+	}
 
-    public String getLearningMethod() {
-        return learningMethod;
-    }
+	public String getLearningMethod() {
+		return learningMethod;
+	}
 
-    public void setLearningMethod(String learningMethod) {
-        this.learningMethod = learningMethod;
-    }
+	public void setLearningMethod(String learningMethod) {
+		this.learningMethod = learningMethod;
+	}
 
-    public int getIncome() {
-        return income;
-    }
+	public int getIncome() {
+		return income;
+	}
 
-    public void setIncome(int income) {
-        this.income = income;
-    }
+	public void setIncome(int income) {
+		this.income = income;
+	}
 
-    public int getDebt() {
-        return debt;
-    }
+	public int getDebt() {
+		return debt;
+	}
 
-    public void setDebt(int debt) {
-        this.debt = debt;
-    }
+	public void setDebt(int debt) {
+		this.debt = debt;
+	}
 
-    public String getGeneral() {
-        return general;
-    }
+	public String getGeneral() {
+		return general;
+	}
 
-    public void setGeneral(String general) {
-        this.general = general;
-    }
+	public void setGeneral(String general) {
+		this.general = general;
+	}
 
-    public boolean isProfileStatus() {
-        return profileStatus;
-    }
+	public boolean isProfileStatus() {
+		return profileStatus;
+	}
 
-    public void setProfileStatus(boolean profileStatus) {
-        this.profileStatus = profileStatus;
-    }
+	public void setProfileStatus(boolean profileStatus) {
+		this.profileStatus = profileStatus;
+	}
 }
