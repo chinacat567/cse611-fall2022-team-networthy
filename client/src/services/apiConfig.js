e314bdaf92058d866b07c44254423533a6f1b133
= <amolghar@buffalo.edu>
2022-11-11 18:21:12 -0500

    Reverting changes

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -2,7 +2,7 @@ export const EMAIL_API = "https://api.emailjs.com/api/v1.0/email/send";
 export const EMAIL_API_2 =
   "https://emailvalidation.abstractapi.com/v1/" +
   process.env.REACT_APP_ABSTRACT_API_KEY;
-export const BASE_URI = "http://ec2-3-19-61-63.us-east-2.compute.amazonaws.com:8080";
+export const BASE_URI = "http://localhost:8080";
 export const API = {
   SIGNUP: "/api/auth/signup",
   LOGIN: "/api/auth/signin",
