6e1019fd374809fc3df084fc68b79c5cbed42aa3
Rahul Sharma <rahulsharma@Rahuls-MacBook-Air-4.local>
2022-11-23 14:45:59 +0530

    FEAT: Landing page UI change

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -5,7 +5,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import landingImg from "../../assets/Images/NWlanding.webp";
+import NWBack from "../../assets/Images/NWBack.png";
 
 import "../../styles/home.scss";
 
@@ -15,20 +15,21 @@ const Home = ({ authWizard }) => {
       <Card sx={{ maxWidth: 600, minHeight: 400 }} className="home__card">
         <CardMedia
           component="img"
-          height="350"
-          src={landingImg}
+          height="400"
+          src={NWBack}
           alt="NWlanding"
+          sx={{ transform: "scale(0.8)" }}
         />
         <CardContent>
           <Typography
             textAlign="center"
             gutterBottom
-            variant="h6"
+            fontWeight={500}
             component="div"
             marginTop={"16px"}
             marginBottom={"24px"}
           >
-            Making Personal Finance
+            Stop dreaming about financial freedom and start living it!
           </Typography>
           <Typography
             variant="body2"
@@ -36,10 +37,18 @@ const Home = ({ authWizard }) => {
             textAlign={"center"}
             fontSize={"15px"}
           >
-            We are a University at Buffalo Startup dedicated to helping young
-            alumni achieve financial wellness. We forge close relationships with
-            our clients and work with them throughout their journey towards
-            achieving financial freedom.
+            We are on a mission to help GenZ achieve its financial goals.
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            textAlign={"center"}
+            fontSize={"15px"}
+            marginTop={"8px"}
+          >
+            Most people have a vision but don’t possess the financial education,
+            guidance, or accountability needed to achieve it. Goally Coaches can
+            help you achieve your most ambitious goals now.
           </Typography>
         </CardContent>
       </Card>
