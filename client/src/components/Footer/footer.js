67401b5bae9e79c7895dc414dd735a037d2c03e1
Rahul Sharma <rahulsharma@Rahuls-MacBook-Air-4.local>
2022-12-04 15:43:19 -0500

    FEAT: Footer Links

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import PhoneIcon from "@mui/icons-material/Phone";
 import MarkunreadIcon from "@mui/icons-material/Markunread";
@@ -8,8 +6,11 @@ import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import TikTokIcon from "../../assets/Icons/tiktok.svg";
 
 import "../../styles/footer.scss";
+import { Link } from "@mui/material";
 
 export default function App() {
   return (
@@ -32,10 +33,27 @@ export default function App() {
       <div className="footer__center">&copy; 2022 By Goally</div>
 
       <div className="footer__right">
-        <FacebookRoundedIcon className="socialIcon" />
-        <LinkedInIcon className="socialIcon" />
-        <TwitterIcon className="socialIcon" />
-        <InstagramIcon className="socialIcon" />
+        <Link href="https://www.instagram.com/goallyfinance/" target="_blank">
+          <InstagramIcon className="socialIcon" />
+        </Link>
+        <Link
+          href="https://www.tiktok.com/@goallyfinance?lang=en"
+          target="_blank"
+        >
+          <img src={TikTokIcon} className="socialIcon" color="blue" />
+        </Link>
+        <Link
+          href="https://www.youtube.com/channel/UCghUWvKFkF4ODVSlWiYqp3Q"
+          target="_blank"
+        >
+          <YouTubeIcon className="socialIcon" />
+        </Link>
+        <Link href="https://twitter.com/GoallyFinance" target="_blank">
+          <TwitterIcon className="socialIcon" />
+        </Link>
+        <Link href="#">
+          <LinkedInIcon className="socialIcon" />
+        </Link>
       </div>
     </footer>
   );
